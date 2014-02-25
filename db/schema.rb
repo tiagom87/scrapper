708b9eaee476381b13b5149223c30a03bfec4796
Tiago Martins <tiago@headblendr.com>
2014-02-25 13:26:41 +0000

    Added heroku compatibility

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140225130019) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "udemies", force: true do |t|
     t.string   "title"
     t.string   "description"
